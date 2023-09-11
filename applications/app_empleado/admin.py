e5b79206dc0d7c03d7a7acd97c487645a1dcfb64
Joaquin <joaquinacholopez@gmail.com>
2023-09-11 17:44:33 -0300

    sass y admin modificado

--- applications/app_empleado/admin.py
+++ applications/app_empleado/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Empleado
+from .models import Empleado, Abilities
 
 # Register your models here.
 
-admin.site.register(Empleado)
+admin.site.register(Abilities)
+
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'job',
+        'department'
+    )
+
+admin.site.register(Empleado,EmpleadoAdmin)
--- applications/app_empleado/models.py
+++ applications/app_empleado/models.py
@@ -2,8 +2,18 @@ from django.db import models
 from applications.app_departamento.models import Departamento
 # Create your models here.
 
-class Empleado(models.Model) :
+class Abilities(models.Model) :
+    ability = models.CharField('Habilidad', max_length = 30)
     
+    class Meta:
+        ordering = ['ability']
+        verbose_name = 'Habilidad'
+        verbose_name_plural = 'Habilidades del empleado'
+        
+    def __str__(self):
+        return self.ability
+
+class Empleado(models.Model) :
     JOB_CHOICES = (
         ('0', 'Contador'),
         ('1', 'Administrativo'),
@@ -16,9 +26,12 @@ class Empleado(models.Model) :
     last_name = models.CharField('Apellido', max_length=20)
     job = models.CharField('Puesto', max_length=1, choices=JOB_CHOICES)
     department = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    abilities = models.ManyToManyField(Abilities)
 
     class Meta:
-        ordering = ['first_name']
+        ordering = ['-first_name', 'last_name']
+        verbose_name = 'Mi empleado'
+        verbose_name_plural = 'Empleados de la empresa'
     
     def __str__(self):
         return self.first_name + ' ' + self.last_name
